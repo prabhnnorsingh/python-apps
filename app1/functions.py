53e3020d0208b2effc5c7b1a4ea07fd2bee1a915
prabhnoor singh <sprabhnoor720@gmail.com>
2023-06-04 22:02:33 +0530

    implement add button

--- app1/functions.py
+++ app1/functions.py
@@ -1,11 +1,12 @@
 # Functions of to-do.py.
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
     """Reads the text file and returns the list of to-do items"""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todo(todos_arg, filepath="todos.txt"):
+def write_todo(todos_arg, filepath=FILEPATH):
     """Write the to-do items list in the text file"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
