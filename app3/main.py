0634655a47f5723eef9ed591135755bedf4348d8
prabhnoor singh <sprabhnoor720@gmail.com>
2023-09-05 17:56:05 +0530

    Add lines

--- app3/main.py
+++ app3/main.py
@@ -9,14 +9,14 @@ df = pd.read_csv("topics.csv")
 for index,row in df.iterrows():
     pdf.add_page()
 
-    #Set Header
+    # Set Header
     pdf.set_font(family="Times", style = "B", size =12)
     pdf.set_text_color(100,100,100)    #The combination of 100's provides grey colour
     pdf.cell(w=0, h=12, txt = row["Topic"], align="L", ln=1)
     for y in range(20,298,10):
         pdf.line(10,y,200,y)
 
-    #Set Footer
+    # Set Footer
     pdf.ln(265)
     pdf.set_font(family="Times", style = "I", size =8)
     pdf.set_text_color(180, 180, 180)
